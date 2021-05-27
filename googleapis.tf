37143d5ba46ddc2927e13bf871e4b9c03d0a0632
dominic <gkstjqdl50@gmail.com>
2021-05-27 09:53:20 +0900

    modifty googleapis by dominic

--- googleapis.tf
+++ googleapis.tf
@@ -3,16 +3,12 @@ resource "google_project_service" "googleapis" {
   for_each = toset([
 "iam.googleapis.com",
 "compute.googleapis.com",
-"storage.googleapis.com",
 "cloudresourcemanager.googleapis.com",
 "stackdriver.googleapis.com",
 "monitoring.googleapis.com",
-"cloudscheduler.googleapis.com",
-"cloudfunctions.googleapis.com",
-"pubsub.googleapis.com",
 ])
 
   service = each.key
-  disable_on_destroy = false
-  disable_dependent_services = true
+  disable_on_destroy = false        #
+  disable_dependent_services = true #지정된 서비스 이외의 종속된 서비스 중지에 대한 설정
 }
